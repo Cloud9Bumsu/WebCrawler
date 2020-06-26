0d254e61bd12f53a02e048b009e558bea33a0ebb
박범수 <38879149+Cloud9Bumsu@users.noreply.github.com>
2020-06-26 18:32:31 +0900

    동국대 VOD Crawling

--- parser.py
+++ parser.py
@@ -4,20 +4,23 @@ from bs4 import BeautifulSoup
 import json
 import os
 
+
 ## python파일의 위치
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-req = requests.get('https://cloud9bumsu.github.io/')
+req = requests.get('https://media.dongguk.edu/multilive/portal/index/main.php?dirnm=media_sin&filenm=media&s_kind1=all&s_kind2=1&s_kind3=subject&s_word=&num_per_page=50&page=1', verify=False)
+req.encoding=
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
 my_titles = soup.select(
-    'h2 > a'
+    'p > b > a > font'
     )
 
 data = {}
 
 for title in my_titles:
-    data[title.text] = title.get('href')
+    print(title.text)
+    data[title.text] = title.text
 
 with open(os.path.join(BASE_DIR, 'result.json'),'w+') as json_file:
     json.dump(data,json_file)
